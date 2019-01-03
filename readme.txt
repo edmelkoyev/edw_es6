d4be6108a623eb5d6116c5cff7a2977567b2a79b
Edward Melkoyev <edmelkoyev@wiley.com>
2019-01-03 05:07:52 -0800

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,5 @@
+npm init -y
+
+Babel
+------------
+$ npm i babel-cli babel-core babel-preset-es2015 --save-dev
---
+++ src/01_let.js
@@ -0,0 +1,6 @@
+if (true) {
+  const version = 'ES6';
+  console.log(`This is internal usage of variable ${version}`);
+}
+
+console.log(version);
